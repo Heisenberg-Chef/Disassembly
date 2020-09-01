7c897501754adabdb52906af92f878debb0ccf8e
Ray <825958926@qq.com>
2020-09-01 11:40:31 +0800

    20200901

---
+++ dll_aka_so/func.h
@@ -0,0 +1,12 @@
+#ifndef _FUNC_H
+#define _FUNC_H
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+int max(int a,int b);
+int add(int a,int b);
+#ifdef __cplusplus
+}
+#endif
+#endif // TEST_H
---
+++ dll_aka_so/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "func.h"
+int main(){
+    printf("max=%d\n",max(4,5));
+    printf("add=%d\n",add(4,5));
+    return 0;
+}
